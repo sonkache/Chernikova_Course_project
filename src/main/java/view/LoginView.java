e7c26e5a80ce360728b9ff488d96257bc8157760
Sofia <sonkache@mail.ru>
2025-06-24 15:17:41 +0300

    Написан класс LoginView - окно авторизации

---
+++ src/main/java/view/LoginView.java
@@ -0,0 +1,44 @@
+package view;
+
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class LoginView {
+    private TextField login = new TextField();
+    private PasswordField password = new PasswordField();
+    private Button bLogin = new Button("Войти");
+    private Button bRegister = new Button("Регистрация");
+
+    public void start(Stage stage) {
+        VBox root = new VBox(10, new Label("Логин:"), login, new Label("Пароль:"), password);
+        HBox hbox = new HBox(10, bLogin, bRegister);
+        root.setPadding(new javafx.geometry.Insets(20));
+        stage.setTitle("Авторизация");
+        stage.setScene(new Scene(root, 400, 400));
+        stage.show();
+    }
+
+    public TextField getLogin(){
+        return login;
+    }
+
+    public PasswordField getPassField(){
+        return password;
+    }
+
+    public Button getLoginButton(){
+        return bLogin;
+    }
+
+    public Button getRegisterButton(){
+        return bRegister;
+    }
+
+    public void error(String msg) {
+        Alert alert = new Alert(Alert.AlertType.ERROR, msg, ButtonType.OK);
+        alert.showAndWait();
+    }
+}
