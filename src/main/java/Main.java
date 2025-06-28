95418a55576f77ef20384693730cbc09b1bd97bc
Sofia <sonkache@mail.ru>
2025-06-28 15:26:32 +0300

    Исправлен класс Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,3 @@
-import database.Database;
 import model.Clients;
 import model.Users;
 import model.CreditTypes;
@@ -10,30 +9,16 @@ import view.LoginView;
 import controller.LoginController;
 
 public class Main extends Application {
-    private Clients clientsModel = new Clients();
-    private Users usersModel = new Users();
-    private CreditTypes creditTypesModel = new CreditTypes();
-    private CreditContracts creditContractsModel = new CreditContracts();
-    private Payments paymentsModel = new Payments();
+    private final Clients clientsModel = new Clients();
+    private final Users usersModel = new Users();
+    private final CreditTypes creditTypesModel = new CreditTypes();
+    private final CreditContracts creditContractsModel = new CreditContracts();
+    private final Payments paymentsModel = new Payments();
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        loadData();
-        LoginView view = new LoginView();
-        LoginController controller = new LoginController(view, clientsModel, usersModel, creditTypesModel, creditContractsModel, paymentsModel);
-        view.start(primaryStage);
-    }
-
-    private void loadData() {
-        try {
-            clientsModel.setAll(Database.loadClients());
-            usersModel.setAll(Database.loadUsers());
-            creditTypesModel.setAll(Database.loadCreditTypes());
-            creditContractsModel.setAll(Database.loadCreditContracts());
-            paymentsModel.setAll(Database.loadPayments());
-        } catch (Exception e) {
-            ;
-        }
+    public void start(Stage primaryStage) {
+        LoginView loginView = new LoginView();
+        LoginController loginController = new LoginController(loginView, clientsModel, usersModel, creditTypesModel, creditContractsModel, paymentsModel);
+        loginView.start(primaryStage);
     }
 
     public static void main(String[] args) {
